334778a5bdfb774129baa823b7bbf3460bb98037
ChangYong <lcy9002@naver.com>
2019-05-25 21:49:52 +0900

    header

--- src/main/java/com/yong/temp/controller/TempController.java
+++ src/main/java/com/yong/temp/controller/TempController.java
@@ -9,8 +9,8 @@ public class TempController {
 	@GetMapping("/")
 	public ResponseEntity<String> test() {
 		return ResponseEntity.ok()
-			.header("Keep-Alive", "timeout=0")
-			.header("Connection", "close")
+			.header("Keep-Alive", "timeout=5")
+			.header("Connection", "keep-alive")
 			.body("hello world");
 	}
 }
